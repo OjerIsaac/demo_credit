0c36a36864610552ff1f01b3542d1ffbfcfc9ed9
Ojer Isaac <isaacojerumu@gmail.com>
2023-01-13 23:59:29 +0100

    feat: knexjs db connection

---
+++ src/knexfile.ts
@@ -0,0 +1,28 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { Knex } from 'knex';
+
+console.log("Service connected to " + process.env.POSTGRES_DB + " successfully");
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.POSTGRES_HOST,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB
+    },
+    migrations: {
+      extension: 'ts',
+      directory: 'db/migrations',
+      tableName: 'migrations_history',
+    },
+    seeds: {
+      extension: 'ts',
+      directory: 'db/seeds',
+    },
+  },
+};
+
+export default config;
